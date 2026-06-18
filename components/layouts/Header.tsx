079c3ba5258678388d65b402b924a666529fa44e
Shobhini <shobhinikundu@gmail.com>
2026-06-18 17:59:40 +0530

    Header ui updated

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,10 +1,25 @@
 "use client"
 import React from 'react'
 import "./Header.css"
+import { FiHome } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa6";
+import { LuShoppingCart } from "react-icons/lu";
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <div className= "box_1">
+        <div className='icon_1'><FiHome /></div>
+        <div className="title">ShopEase</div> 
+      </div>
+        <div className="box_2">
+          <div className="icon_2">
+            <FaRegUser />
+          </div>
+          <div className="icon_3"><LuShoppingCart />
+          </div>
+      </div>
+    </div>
   )
 }
 
