c827628aa3c0873894e0072d1182eed5731f4933
Shobhini <shobhinikundu@gmail.com>
2026-06-26 16:46:39 +0530

    Category Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import HomeBanner from "@/components/homeComponents/HomeBanner";
+import HomeCategory from "@/components/homeComponents/HomeCategory";
 import Header from "@/components/layouts/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (<>
       <Header/>
       <HomeBanner/>
+      <HomeCategory/>
     </>
   );
 }
---
+++ interfaces/products.ts
@@ -0,0 +1,5 @@
+export interface ICatergory {
+    id: number;
+    name: string;
+    image: string;
+}
